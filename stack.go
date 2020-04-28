1c126af10ff04d3071c0b84c1eb4395536de31cd
Andrew Wong <andy.h.wong@gmail.com>
2020-04-28 16:42:43 -0400

    20200428 at 1642 EST

--- stack.go
+++ stack.go
@@ -1,16 +1,23 @@
 package stack
 
 //
-type Stack []byte
+type Stack struct {
+	body []byte
+}
+
+//
+func New() *Stack {
+	return &Stack{}
+}
 
 // IsEmpty: check if stack is empty
 func (s *Stack) IsEmpty() bool {
-	return len(*s) == 0
+	return len((*s).body) == 0
 }
 
 // Push a new value onto the stack
 func (s *Stack) Push(bt byte) {
-	*s = append(*s, bt) // Simply append the new value to the end of the stack
+	(*s).body = append((*s).body, bt) // Simply append the new value to the end of the stack
 }
 
 // Remove and return top element of stack. Return false if stack is empty.
@@ -19,9 +26,9 @@ func (s *Stack) Pop() byte {
 		return 0
 	}
 
-	index := len(*s) - 1   // Get the index of the top most element.
-	element := (*s)[index] // Index into the slice and obtain the element.
-	*s = (*s)[:index]      // Remove it from the stack by slicing it off.
+	index := len((*s).body) - 1   // Get the index of the top most element.
+	element := (*s).body[index]   // Index into the slice and obtain the element.
+	(*s).body = (*s).body[:index] // Remove it from the stack by slicing it off.
 	return element
 }
 
@@ -31,5 +38,5 @@ func (s *Stack) Peek() byte {
 		return 0
 	}
 
-	return (*s)[len(*s)-1]
+	return (*s).body[len((*s).body)-1]
 }
